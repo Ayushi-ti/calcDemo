6be8c6e239ddec35e976a62962c9df693e308cd0
Ayushi <AYUSHI.ayushi@ibm.com>
2021-03-09 13:20:34 +0530

    first commit

---
+++ src/com/demo/basicCalc.java
@@ -0,0 +1,9 @@
+package com.demo;
+
+public class basicCalc {
+public static int Add(int a, int b) {
+	return a+b;
+}
+	
+	
+}
---
+++ src/com/demo/scientificCalc.java
@@ -0,0 +1,8 @@
+package com.demo;
+
+public class scientificCalc {
+public static double CalcSqrt(int num) {
+	return Math.pow(num, 0.5);
+	Assert.assertEquals()
+}
+}
